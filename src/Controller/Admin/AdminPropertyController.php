fdd24c00232f82f57e7464b8a92c46114556e686
Grégory Marini <greg.marini@hotmail.fr>
2022-06-30 19:10:35 +0200

    creation table et creation entité property première migration ok

---
+++ src/Controller/Admin/AdminPropertyController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminPropertyController extends AbstractController
+{
+    public function __construct()
+    {
+        
+    }
+
+    public function index()
+    {
+
+    }
+}
